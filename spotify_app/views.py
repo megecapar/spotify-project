b1a33c82c5150d41b13de48f6cd7735f168bc7e1
megecapar <egecapar08@gmail.com>
2022-08-06 16:08:43 +0300

    now oauth key is passed from pastebin, we can edit it if it expires

--- spotify_app/views.py
+++ spotify_app/views.py
@@ -9,17 +9,21 @@ import json
 
 # Create your views here.
 url = 'https://api.spotify.com/v1/browse/featured-playlists'
-OAuthKey = "BQDxndovNrKJdfO-0HQWkRg00WQHIMYENGAZvp3wGAX-1GpDDZttXrxSpQwyqjWB2xl_Pj8vEAFSJKcqvQKiVjnSfRUzRe0UjKMy24OEjaosmd5CwL3Dd66ox7nehvrMcWYuIgjtfT7PtJpn0NOUO1hqnpLpirO-cLjDJNczfUJd55mKjjH3G1pAw5MKoa5-z_c"
+#OAuthKey = "BQDxndovNrKJdfO-0HQWkRg00WQHIMYENGAZvp3wGAX-1GpDDZttXrxSpQwyqjWB2xl_Pj8vEAFSJKcqvQKiVjnSfRUzRe0UjKMy24OEjaosmd5CwL3Dd66ox7nehvrMcWYuIgjtfT7PtJpn0NOUO1hqnpLpirO-cLjDJNczfUJd55mKjjH3G1pAw5MKoa5-z_c"
 
 
+def get_oauthKey():
+    key =  requests.get("https://pst.klgrth.io/paste/ksv6q/raw").text
+    return key
 def get_playlists(request: object) -> object:
-    response = requests.get(url, headers={"Authorization": "Bearer " + OAuthKey})
-    data = response.json()["playlists"]["items"]
-    for playlist in data:
-        if not Playlist.objects.filter(name=playlist["name"]).exists():
-            Playlist.objects.create(name=playlist["name"],owner=playlist["owner"]["display_name"],isCollaborative=playlist["collaborative"],description=playlist["description"],url=playlist["external_urls"]["spotify"])
-    print(Playlist.objects.all())
-    return render(request, "playlists.html", {"response": Playlist.objects.all()})
+    response = requests.get(url, headers={"Authorization": "Bearer " + get_oauthKey()})
+    if response.status_code != 401:
+        data = response.json()["playlists"]["items"]
+        for playlist in data:
+            if not Playlist.objects.filter(name=playlist["name"]).exists():
+                Playlist.objects.create(name=playlist["name"],owner=playlist["owner"]["display_name"],isCollaborative=playlist["collaborative"],description=playlist["description"],url=playlist["external_urls"]["spotify"])
+        print(Playlist.objects.all())
+        return render(request, "playlists.html", {"response": Playlist.objects.all()})
 
 
 class PlaylistView(generic.ListView):
@@ -30,14 +34,16 @@ class PlaylistView(generic.ListView):
 
 
 class PlaylistUpdateView(generic.ListView):
-    response = requests.get(url, headers={"Authorization": "Bearer " + OAuthKey})
-    data = response.json()["playlists"]["items"]
-    for playlist in data:
-        if not Playlist.objects.filter(name=playlist["name"]).exists():
-            Playlist.objects.create(name=playlist["name"], owner=playlist["owner"]["display_name"],
-                                    isCollaborative=playlist["collaborative"], description=playlist["description"],
-                                    url=playlist["external_urls"]["spotify"])
-
+    print("Authorization:" "Bearer " + get_oauthKey())
+    response = requests.get(url, headers={"Authorization": "Bearer " + get_oauthKey()})
+    if response.status_code != 401:
+        data = response.json()["playlists"]["items"]
+        for playlist in data:
+            if not Playlist.objects.filter(name=playlist["name"]).exists():
+                Playlist.objects.create(name=playlist["name"], owner=playlist["owner"]["display_name"],
+                                        isCollaborative=playlist["collaborative"], description=playlist["description"],
+                                        url=playlist["external_urls"]["spotify"])
+    else: print(response)
 
     model = Playlist
     template_name = "playlists.html"
